aee8136d9e58d8f33332c575e08f8209a38dfe5f
saidkayed <36620432+saidkayed@users.noreply.github.com>
2018-11-14 10:47:16 +0100

    hey

--- Ca3Frontend/src/Swapi.js
+++ Ca3Frontend/src/Swapi.js
@@ -4,19 +4,22 @@ import facade from "./apiFacade";
 export default class Swapipeople extends Component{
     constructor(props){
         super(props);
-        this.state = { dataFromServer: "Fetching!!" };
+        this.state = { dataFromServer: [] };
 
     }
-    componentDidMount(){
-    facade.fetchSwapiPeople().then(res => this.setState({ dataFromServer: res }))
+    async componentDidMount (){
+    const peps = await fetch("http://localhost:8080/jwtbackend/api/swapi/people").then(res =>res.json());
    //    console.log(this.state.dataFromServer);
+   this.state.dataFromServer.push(peps);
+   this.setState({dataFromServer:this.state.dataFromServer})
+   console.log(this.state.dataFromServer);
     }
     
     render(){    
     return(
         <div>
-        {this.state.dataFromServer.results.map((data) => 
-            data.results.name
+        {this.state.dataFromServer.map((data) => 
+       <h1> {data.results[0].name}</h1>
         )
     }
         </div>
