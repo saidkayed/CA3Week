a57fc1c60c7fa9cab771197935f844fc1f293c4e
saidkayed <36620432+saidkayed@users.noreply.github.com>
2018-11-15 11:38:59 +0100

    hej

--- Ca3Frontend/src/Pagination.js
+++ Ca3Frontend/src/Pagination.js
@@ -1,10 +1,34 @@
 import React, {Component} from 'react';
+import facade from "./apiFacade";
 
 export default class pagination extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {pagination_data: []}
+    }
+    componentDidMount(){
+        const peps = facade.fetch_dummie_data();
+
+        this.setState = {pagination_data: peps}
+    }
     render(){
         return(
             <div>
-                
+                 <h3 align="center">People</h3>
+                <table id="table">
+                    <thead>
+                        <tr></tr>
+                    </thead>
+                    <tbody>
+                        {this.state.pagination_data.map((data) =>
+                            <tr key={data.id}>
+                            <th> {data.name}</th>
+                            
+                            </tr>
+                        )}
+                    </tbody>
+                </table> 
 
 
 
